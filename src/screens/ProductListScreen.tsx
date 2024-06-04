ea2221f107c8261864922a96b29cd57d168c7fa9
Eren AVAR <eren.avar@hotmail.com>
2024-06-04 17:35:03 +0200

    create folders and add some react core components

---
+++ src/screens/ProductListScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ProductListScreen() {
+    return (
+        <View>
+            <Text>ProductListScreen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
